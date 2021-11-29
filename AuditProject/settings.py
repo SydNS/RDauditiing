a8b2cec4d232cb2ac6bf873da387fc04a76594ee
SydNS <sydneynsiimbe@gmail.com>
2021-11-29 16:28:31 +0300

    added dist files

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# STATIC_DIR = os.path.join(BASE_DIR, '/governanceandcontrol/static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
