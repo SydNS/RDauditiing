48183bcf375a085096973534c64709478a48a88a
SydNS <sydneynsiimbe@gmail.com>
2021-12-01 21:13:15 +0300

    removed leadinig "/"

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
