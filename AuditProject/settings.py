395ba3e67e627ef9da36790ee95a028cb6b2325c
SydNS <sydneynsiimbe@gmail.com>
2021-12-01 20:37:52 +0300

    added many hosts

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -25,13 +25,11 @@ SECRET_KEY = '&@twfq=^f$*xw)^ithgpgtcfh3y7$h7ej0+_=oz4x!k_(130^e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
-
-    'whitenoise.runserver_nostatic',
     'governanceandcontrol',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -112,17 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '/static/'),
-# ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
