0a977bc229a333dcf0fe13ef13a1298649b17372
SydNS <sydneynsiimbe@gmail.com>
2021-12-01 20:24:36 +0300

    added many hosts

--- AuditProject/urls.py
+++ AuditProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('audit/', include('governanceandcontrol.urls')),
+    path('', include('governanceandcontrol.urls')),
 
 ]
