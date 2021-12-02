439e1c22455099be6a8d15dace156d6f171f74df
SydNS <sydneynsiimbe@gmail.com>
2021-12-02 18:49:09 +0300

    modified the order of the installed apps

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -30,8 +30,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'whitenoise.runserver_nostatic',
+    'jazzmin',
     'governanceandcontrol',
     'django.contrib.admin',
     'django.contrib.auth',
