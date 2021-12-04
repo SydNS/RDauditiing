6a12aa20e24d408360a634551f781a9cbf476b11
SydNS <sydneynsiimbe@gmail.com>
2021-12-05 00:13:08 +0300

    registerd the accounts app

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'jazzmin',
     'governanceandcontrol',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
