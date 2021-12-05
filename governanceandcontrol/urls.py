24436975ccd7af5c805a3a79f7f6df60fabfd5d5
SydNS <sydneynsiimbe@gmail.com>
2021-12-05 03:28:51 +0300

    using namespaces in urls now to simplify it

--- governanceandcontrol/urls.py
+++ governanceandcontrol/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
-    path('login/', views.loginuser, name='dashboard'),
+    path('login/', views.loginuser, name='login'),
     path('register/', views.Registering, name='register'),
 
 ]
