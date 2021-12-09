74fbd18b90dc383f999055460604e4608b48ed63
SydNS <sydneynsiimbe@gmail.com>
2021-12-09 11:05:27 +0300

    added the risk model to admin

--- governanceandcontrol/admin.py
+++ governanceandcontrol/admin.py
@@ -25,16 +25,17 @@ class GncTableAdmin(admin.ModelAdmin):
     search_fields = ('audit_project_code', 'audit_project_name', 'quarter',
                      'criticality', 'owner',
                      )
+
 @admin.register(RiskManagement)
 class RiskManagementAdmin(admin.ModelAdmin):
     list_display = ('xy_strategic_pillar', 'xy_strategic_objectives', 'risk_category', 'risk_description',
                     'likelihood', 'impact', 'risk_rating', 'control_exists_yesno',
-                    'control_description', 'control_is_adequate_yesno', 'recommended_control', 'revised_implementation_date'
-                    , 'last_status_update', 'ageing_days', 'actual_implementation_date', 'action',
+                    'control_description', 'control_is_adequate_yesno', 'recommended_control'
+                    ,  'action',
                     'kri','target','dept',
                     )
-    list_filter = ( 'xy_strategic_pillar', 'ageing_days',
-                   'actual_implementation_date',
+    list_filter = ( 'xy_strategic_pillar',
+                   'control_description','xy_strategic_objectives','risk_category',
                    )
     search_fields = ('xy_strategic_pillar', 'xy_strategic_objectives', 'dept',
                      'impact', 'target',
