738213a13e2e12dc3f706a16a68bd6d3cc84857c
SydNS <sydneynsiimbe@gmail.com>
2021-12-11 17:50:12 +0300

    gnc form

--- governanceandcontrol/forms.py
+++ governanceandcontrol/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from . import models
 
 # Create your forms here.
+from .models import GncTable
+
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -18,3 +20,13 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class GncTableForm(forms.Form):
+    class Meta:
+        model = GncTable
+        fields = ['audit_project_code', 'audit_project_name', 'quarter', 'audit_project_type',
+                  'issue_title', 'criticality', 'root_cause_analysis', 'recommendation',
+                  'action_plan', 'recommendation_state', 'agreed_implementation_date', 'revised_implementation_date'
+            , 'last_status_update', 'ageing_days', 'actual_implementation_date', 'owner', 'final_approver',
+                  ]
