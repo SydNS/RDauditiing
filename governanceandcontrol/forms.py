aef61ed711476ff8a50fb5d9fbe8333bc3902f59
SydNS <sydneynsiimbe@gmail.com>
2021-12-15 11:23:36 +0300

    moved the forms to the accounts app

--- governanceandcontrol/forms.py
+++ governanceandcontrol/forms.py
@@ -7,19 +7,19 @@ from . import models
 from .models import GncTable
 
 
-class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+# class NewUserForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#
+#     class Meta:
+#         model = User
+#         fields = ("username", "email", "password1", "password2")
+#
+#     def save(self, commit=True):
+#         user = super(NewUserForm, self).save(commit=False)
+#         user.email = self.cleaned_data['email']
+#         if commit:
+#             user.save()
+#         return user
 
 
 class GncTableForm(forms.ModelForm):
