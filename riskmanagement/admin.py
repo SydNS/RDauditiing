7896c56b32c588d74cbfe68319c8901abbe804d6
SydNS <sydneynsiimbe@gmail.com>
2021-12-15 11:26:15 +0300

    register risk models in admin

--- riskmanagement/admin.py
+++ riskmanagement/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 
 # Register your models here.
+# Register your models here.
+from .models import RiskManagement
 
 @admin.register(RiskManagement)
 class RiskManagementAdmin(admin.ModelAdmin):
