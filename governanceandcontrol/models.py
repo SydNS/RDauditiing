bf3d4c64bc7dc23465212e8a5e333f8f69ff7f88
SydNS <sydneynsiimbe@gmail.com>
2021-12-15 11:21:11 +0300

    moved the dept model and person model

--- governanceandcontrol/models.py
+++ governanceandcontrol/models.py
@@ -1,41 +1,5 @@
 from django.db import models
 
-class Dept(models.Model):
-    CRITICALITY_LEVELS = (
-        ('low', 'low'),
-        ('medium', 'medium'),
-        ('high', 'high'),
-    )
-    deptname = models.CharField(max_length=100)
-    deptrole = models.CharField(max_length=100)
-    numberofmembers = models.IntegerField()
-    criticality = models.CharField(db_column='Criticality', max_length=20, blank=False, null=True,
-                                   choices=CRITICALITY_LEVELS, default='draft')  # Field name made lowercase.
-    def __str__(self):
-        return self.deptname
-
-
-    # class Meta:
-    #     unique_together = ("deptname", "deptrole",)
-
-
-class Person(models.Model):
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    personemail = models.EmailField()
-    personsdept = models.ForeignKey(Dept, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = "People"
-
-    def __str__(self):
-        return self.last_name
-
-
-class GradingUser(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    grade = models.PositiveSmallIntegerField()
-    deptnem = models.ForeignKey(Dept, on_delete=models.CASCADE)
 
 
 class GncTable(models.Model):
@@ -67,39 +31,23 @@ class GncTable(models.Model):
         ('Corporate Affairs', 'Corporate Affairs'),
     )
     
-    audit_project_code = models.CharField(db_column='Audit_Project_Code', max_length=100, blank=False,
-                                          null=True)  # Field name made lowercase.
-    audit_project_name = models.CharField(db_column='Audit_Project_Name', max_length=100, blank=False,
-                                          null=True)  # Field name made lowercase.
-    quarter = models.CharField(db_column='Quarter', choices=QUARTERS, max_length=100, blank=False,
-                               null=True)  # Field name made lowercase.
-    audit_project_type = models.CharField(db_column='Audit_Project_Type', max_length=100, blank=True,choices=PROJECT_TYPE,
-                                          null=True)  # Field name made lowercase.
-    issue_title = models.CharField(db_column='Issue_Title', max_length=50, blank=False,
-                                   null=True)  # Field name made lowercase.
-    criticality = models.CharField(db_column='Criticality', max_length=20, blank=False, null=True,
-                                   choices=CRITICAL_LEVELS, default='draft')  # Field name made lowercase.
-    root_cause_analysis = models.CharField(db_column='Root_Cause_Analysis', choices=ROOT_CAUSE_ANALYSIS, max_length=100,
-                                           blank=False, null=True)  # Field name made lowercase.
-    recommendation = models.CharField(db_column='Recommendation', max_length=100, blank=False,
-
-                                      null=True)  # Field name made lowercase.
-    action_plan = models.CharField(db_column='Action_Plan', max_length=100, blank=False,
-                                   null=True)  # Field name made lowercase.
-    recommendation_state = models.CharField(db_column='Recommendation_State', max_length=100, blank=False, choices=RECOMMENDATION_STATE,
-                                            null=True)  # Field name made lowercase.
-    agreed_implementation_date = models.DateField(db_column='Agreed_Implementation_Date', max_length=100, blank=True,
-                                                  null=True)  # Field name made lowercase.
-    revised_implementation_date = models.DateField(db_column='Revised_Implementation_Date', max_length=100, blank=True,
-                                                   null=True)  # Field name made lowercase.
-    last_status_update = models.DateField(db_column='Last_Status_Update', max_length=100, blank=False,
-                                          null=True)  # Field name made lowercase.
-    ageing_days = models.FloatField(db_column='Ageing__Days', max_length=10, blank=False,
-                                    null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
-    actual_implementation_date = models.DateField(db_column='Actual_Implementation_Date', max_length=100, blank=True,
-                                                  null=True)  # Field name made lowercase.
-    owner = models.ForeignKey(Dept, on_delete=models.CASCADE)     # Field name made lowercase.
-    final_approver = models.ForeignKey(Person, on_delete=models.CASCADE)  # Field name made lowercase.
+    audit_project_code = models.CharField(db_column='Audit_Project_Code', max_length=100, blank=False,null=True)  # Field name made lowercase.
+    audit_project_name = models.CharField(db_column='Audit_Project_Name', max_length=100, blank=False,null=True)  # Field name made lowercase.
+    quarter = models.CharField(db_column='Quarter', choices=QUARTERS, max_length=100, blank=False,null=True)  # Field name made lowercase.
+    audit_project_type = models.CharField(db_column='Audit_Project_Type', max_length=100, blank=True,choices=PROJECT_TYPE,null=True)  # Field name made lowercase.
+    issue_title = models.CharField(db_column='Issue_Title', max_length=50, blank=False,null=True)  # Field name made lowercase.
+    criticality = models.CharField(db_column='Criticality', max_length=20, blank=False, null=True,choices=CRITICAL_LEVELS, default='draft')  # Field name made lowercase.
+    root_cause_analysis = models.CharField(db_column='Root_Cause_Analysis', choices=ROOT_CAUSE_ANALYSIS, max_length=100,blank=False, null=True)  # Field name made lowercase.
+    recommendation = models.CharField(db_column='Recommendation', max_length=100, blank=False,null=True)  # Field name made lowercase.
+    action_plan = models.CharField(db_column='Action_Plan', max_length=100, blank=False,null=True)  # Field name made lowercase.
+    recommendation_state = models.CharField(db_column='Recommendation_State', max_length=100, blank=False, choices=RECOMMENDATION_STATE,null=True)  # Field name made lowercase.
+    agreed_implementation_date = models.DateField(db_column='Agreed_Implementation_Date', max_length=100, blank=True,null=True)  # Field name made lowercase.
+    revised_implementation_date = models.DateField(db_column='Revised_Implementation_Date', max_length=100, blank=True,null=True)  # Field name made lowercase.
+    last_status_update = models.DateField(db_column='Last_Status_Update', max_length=100, blank=False,null=True)  # Field name made lowercase.
+    ageing_days = models.FloatField(db_column='Ageing__Days', max_length=10, blank=False,null=True)  # Field name made lowercase. Field renamed because it contained more than one '_' in a row.
+    actual_implementation_date = models.DateField(db_column='Actual_Implementation_Date', max_length=100, blank=True,null=True)  # Field name made lowercase.
+    owner = models.ForeignKey('accounts.Dept', on_delete=models.CASCADE)     # Field name made lowercase.
+    final_approver = models.ForeignKey('accounts.Person', on_delete=models.CASCADE)  # Field name made lowercase.
 
     # class Meta:
     #     managed = False
