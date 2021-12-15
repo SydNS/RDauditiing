9e5fc9fbec89fae32905652d0f5124100c9ec4f3
SydNS <sydneynsiimbe@gmail.com>
2021-12-15 11:26:34 +0300

    register accounts models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Dept,Person,GradingUser
+
+# class Dept(models.Model):
+#     CRITICALITY_LEVELS = (
+#         ('low', 'low'),
+#         ('medium', 'medium'),
+#         ('high', 'high'),
+#     )
+#     deptname = models.TextField()
+#     deptrole = models.TextField()
+#     deptcriticality = models.TextField()
+#     numberofmembers = models.IntegerField()
+#     criticality = models.CharField(db_column='Criticality', max_length=20, blank=False, null=True,
+#                                    choices=CRITICALITY_LEVELS, default='draft')  # Field name made lowercase.
+
+@admin.register(Dept)
+class DeptAdmin(admin.ModelAdmin):
+    list_display = ('deptname', 'deptrole', 'numberofmembers', 'criticality',)
+    list_filter = ('deptname', 'deptrole',  'numberofmembers', 'criticality',)
+    search_fields = ('deptname', 'deptrole', 'numberofmembers', 'criticality',)
+
+
+# class Person(models.Model):
+#     last_name = models.TextField()
+#     first_name = models.TextField()
+#     personemail = models.EmailField()
+#     personsdept = models.ForeignKey(Dept, on_delete=models.CASCADE)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'personemail', 'personsdept',)
+    list_filter = ('last_name', 'first_name', 'personemail', 'personsdept',)
+    search_fields = ('last_name', 'first_name', 'personemail', 'personsdept',)
+
+
+#
+# class GradingUser(models.Model):
+#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     grade = models.PositiveSmallIntegerField()
+#     deptnem = models.ForeignKey(Dept, on_delete=models.CASCADE)
+#
+@admin.register(GradingUser)
+class GradingUserAdmin(admin.ModelAdmin):
+    list_display = ('person', 'grade', 'deptnem',)
+    list_filter = ('person', 'grade', 'deptnem',)
+    search_fields = ('person', 'grade', 'deptnem',)
