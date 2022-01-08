36b99c1cd26d6c87f070f9053abe5853a92823ca
SydNS <40373744+SydNS@users.noreply.github.com>
2022-01-08 16:07:04 +0300

    db setting

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -91,15 +91,15 @@ WSGI_APPLICATION = 'AuditProject.wsgi.application'
 DATABASES = {
 
 }
-if DEBUG:
-    DATABASES["default"] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# if DEBUG:
+#     DATABASES["default"] ss= {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+# else:
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
