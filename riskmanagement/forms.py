1d5e7e85372d347a8bec6c550334c8c2928c0d74
SydNS <sydneynsiimbe@gmail.com>
2022-01-15 14:10:49 +0300

    made changes to the views and created forms

---
+++ riskmanagement/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from . import models
+
+# Create your forms here.
+from .models import RiskManagement
+
+class RiskManagementForm(forms.ModelForm):
+
+    class Meta:
+        model = RiskManagement
+        fields = "__all__"
+        widgets = {
+            'agreed_implementation_date': forms.DateInput(format=('%m/%d/%Y'),
+                                             attrs={'class': 'form-control', 'placeholder': 'Select a date',
+                                                    'type': 'date'}),
+            'revised_implementation_date': forms.DateInput(format=('%m/%d/%Y'),
+                                                          attrs={'class': 'form-control',
+                                                                 'placeholder': 'Select a date',
+                                                                 'type': 'date'}),
+            'actual_implementation_date': forms.DateInput(format=('%m/%d/%Y'),
+                                                          attrs={'class': 'form-control',
+                                                                 'placeholder': 'Select a date',
+                                                                 'type': 'date'}),
+        }
--- riskmanagement/urls.py
+++ riskmanagement/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     path('riskcontrol/', views.RiskControl, name='riskcontrol'),
     path('keyrateindicator/', views.KRI, name='keyrateindicator'),
 
+    # Risks details
+    path('risks/<int:id>/', views.RisksDetail, name='risksdetails'),
+
+    # addrisk
+    path('addrisk/', views.AddRiskRecords, name='addrisk'),
+
 
 
 ]
