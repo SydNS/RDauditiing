749a0f37623c1035e63c7e65f2bc8fa1a43986b7
SydNS <sydneynsiimbe@gmail.com>
2022-01-15 17:11:25 +0300

    no need for mysql

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -14,8 +14,8 @@ import os
 from decouple import config
 import dj_database_url
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
