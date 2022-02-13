d5b8eca2475dc0a61c3c80543b57546569015ca1
SydNS <sydneynsiimbe@gmail.com>
2022-02-13 06:57:26 +0300

    links to profile information

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
 
     path('login/', views.loginuser, name='login'),
     path('register/', views.Registering, name='register'),
+
+    path('profilesetup/', views.ProfileSetting, name='profilesetup'),
+
+    path('profiledetails/', views.Profiledetails, name='profiledetails'),
+
     path('logout/', views.Logout, name='logout'),
 
     # peopleview
