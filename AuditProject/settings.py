8a05545f866e0ae4d8d2565c77524a6a45589ff2
SydNS <sydneynsiimbe@gmail.com>
2022-02-13 12:06:39 +0300

    changed static root and stat files dirs

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -146,9 +146,9 @@ STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_files_folder'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files_folder')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # media upload
