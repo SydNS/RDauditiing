4b466ef1a905a87464e618069b451dd8d8e7c4d3
SydNS <sydneynsiimbe@gmail.com>
2022-02-14 10:39:43 +0300

    using default variables

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -11,18 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from decouple import config
-
 from pathlib import Path
 import psycopg2
 
-# import pymysql
+import pymysql
 from decouple import config
-#
-# pymysql.install_as_MySQLdb()
 
-# import pymysql
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,7 +30,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&@twfq=^f$*xw)^ithgpgtcfh3y7$h7ej0+_=oz4x!k_(130^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+# DEBUG = config("DEBUG", cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -106,15 +102,6 @@ DATABASES["default"] = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
-
-
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-# else:
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
