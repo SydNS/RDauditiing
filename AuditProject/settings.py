36010c134e2ec6afb39558d0127e028ffc75090e
SydNS <sydneynsiimbe@gmail.com>
2022-02-14 15:20:07 +0300

    collected the statics here

--- AuditProject/settings.py
+++ AuditProject/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files_folder')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # media upload
